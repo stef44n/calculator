d98dc5d88230d38ff557b11e0083927fb7f3cbaf
Stefan <stefanseifeddine@gmail.com>
2022-07-09 19:01:33 +0100

    Add the "divide" function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Main calculator functions
 
-let num1 = 5;
+let num1 = 6;
 let num2 = 3;
 
 const add = function() {
@@ -20,3 +20,9 @@ const multiply = function() {
 };
 
 console.log(`The result of ${num1} x ${num2} = ${multiply()}`)
+
+const divide = function() {
+    return num1 / num2;
+};
+
+console.log(`The result of ${num1} / ${num2} = ${divide()}`)
